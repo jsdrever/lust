86fe25bc8a424977e72f59833edbf01d55272391
jsdrever <jsdrever09@gmail.com>
2023-01-02 20:44:37 -0600

    added alot more for probs no reason

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// generate questions, choices, take score and store it
+// generate questions, choices, take score and store it, and a timer
 
 
 
@@ -56,3 +56,58 @@ startGame = () => {
     availableQuestions = [...questions]
     getNewQuestion()
 }
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.getElementsByClassName.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventlistener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout (() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+
+
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score +=num.scoreText.innerText = scoreText
+}
+
+startGame()
